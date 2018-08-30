736a99442c0a5bab1d62804a6d7ce2ca241c3c2d
charle <jack107t@hotmail.com>
2018-08-30 22:22:52 +0800

    add USA

--- driving.py
+++ driving.py
@@ -2,7 +2,12 @@ country = input('Which country are you from:')
 age = input('pls input your age:')
 age = int(age)
 if country == 'Taiwan':
-	if age >= 18 :
+	if age >= 16 :
+		print('you can test for Driving Licence ')
+	else:
+		print('you can not test for Driving Licence ')
+elif country == 'USA':
+	if age >= 16 :
 		print('you can test for Driving Licence ')
 	else:
 		print('you can not test for Driving Licence ')
